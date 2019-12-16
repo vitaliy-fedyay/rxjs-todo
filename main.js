603dc270b15b3f6f00d8fe64157e36da287b4132
VitaliyFedyay <fedyay.vitaliy@gmail.com>
2019-12-16 17:25:29 +0200

    checking error

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ const search = document.getElementById("search")
 const newElement = document.getElementById("newElement")
 const addElement = document.getElementById("addElement")
 const listElements = document.getElementById("listElements")
+const listElementsSearch = document.getElementById("listElementsSearch")
 
 let data = []
+let list = []
 let cache
 
 fromEvent(newElement, 'input').pipe(
-  debounceTime(1000),
   map(event => event.target.value)
 )
   .subscribe(value => cache = value)
@@ -18,8 +19,31 @@ fromEvent(newElement, 'input').pipe(
 fromEvent(addElement, 'click')
   .subscribe(
     function () {
+      data = []
       data.push(cache)
-      listElements.innerHTML = data
+      list.push(cache)
+      data.forEach(element => {
+        listElements.innerHTML += "<li>" + element + "</li>"
+      });
+    }
+  )
+
+fromEvent(search, 'input').pipe(
+  debounceTime(1000),
+  map(event => event.target.value)
+)
+  .subscribe(
+    function (value) {
+      list.forEach(element => {
+        JSON.stringify(element)
+        JSON.stringify(value)
+        if (element === value) {
+          listElementsSearch.innerHTML = element
+        }
+        else {
+          listElementsSearch.innerHTML = "Element not found"
+        }
+      })
     }
   )
 
